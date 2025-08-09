855dfd37265987dcb0ad3512b63e5e73f3225c69
Mateus <mateusfernandes0927@gmail.com>
2025-08-09 12:50:08 -0300

    subindo os testes

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def create_task():
     tasks.append(new_task)
     print(tasks)
     #retorna em Json na api do postman(Padrão REST)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
